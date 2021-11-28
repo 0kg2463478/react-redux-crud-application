02a9c10cf545a2cbb5c38fd52f695c7b96eeec9e
0kg2463478 <quanzuomu135@gmail.com>
2021-11-28 17:16:00 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    // const dom = <input type="button" onClick={() => {console.log("hello")}} />;
-  	// return dom;
-    return (
-      <React.Fragment>
-        <label>bar</label>
-        <input type="text" onClick={() => {console.log("hello")}} />
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label>bar</label>
+//         <input type="text" onClick={() => {console.log("hello")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return <Cat />
 }
 
+const Cat = () => {
+  return <div>a</div>
+}
 export default App;

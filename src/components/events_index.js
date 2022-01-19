121a02e83b8a830fb9e5379e69132d3006d5483f
0kg2463478 <quanzuomu135@gmail.com>
2022-01-19 23:00:07 +0900

    implements evnts index

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { readEvents } from '../actions';
+import _ from 'lodash'
 
 class EventsIndex extends Component  {
   componentDidMount() {
     this.props.readEvents()
   }
-  render() {
-    const props = this.props
 
+  readEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
+  render() {
     return (
     <React.Fragment>
-      <div>value: { props.value }</div>
-      <button onClick={ props.increment }>+</button>
-      <button onClick={ props.decrement }>-</button>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <thead>
+          {this.readEvents()}
+        </thead>
+      </table>
     </React.Fragment>
     )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({ events : state.events })
 
 const mapDispatchToProps = ({ readEvents })
 

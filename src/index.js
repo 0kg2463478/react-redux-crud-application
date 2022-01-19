31c8a404d23c91feb29e69dd31312dd08856f034
0kg2463478 <quanzuomu135@gmail.com>
2022-01-19 23:59:48 +0900

    画面遷移まで

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer, applyMiddleware(thunk))
@@ -14,7 +16,12 @@ const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Routes>        
+        <Route path='/' element={<EventsIndex/>} />
+        <Route path='/events/new' element={<EventsNew/>} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
